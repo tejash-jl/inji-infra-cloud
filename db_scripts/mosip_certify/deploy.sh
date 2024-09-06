98c911808975ab0a642a08f7fb08b971ade73956
Tejash <tjl1@deloitte.com>
2024-09-06 16:05:16 +0530

    Add db scripts for inji certify
    (cherry picked from commit 30b8510ef84f1112aa12fb8a516a8c2ab43f9629)
    Signed-off-by: Tejash JL <tejash.jl1020@gmail.com>

---
+++ db_scripts/mosip_certify/deploy.sh
@@ -0,0 +1,7 @@
+
+## Properties file
+set -e
+
+## Grants
+PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f init.sql
+
