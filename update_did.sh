4c1a7c43871d892ee4ab5ee3aed6e2029e009ab1
tejash-jl <tejash.jl1020@gmail.com>
2024-10-17 23:19:52 +0530

    Add scripts to update did and p12
    Signed-off-by: tejash-jl <tejash.jl1020@gmail.com>

---
+++ update_did.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+update_did(){
+  cd ~/azure-devops/
+
+
+  echo -n "$1" | xargs -I '{}' sed -i -E 's@mosip.esignet.vciplugin.sunbird-rc.credential-type.VaccinationCredential.static-value-map.issuerId=.*@mosip.esignet.vciplugin.sunbird-rc.credential-type.VaccinationCredential.static-value-map.issuerId={}@' esignet-local.properties
+  echo -n "$1" | xargs -I '{}' sed -i -E 's@mosip.certify.vciplugin.sunbird-rc.credential-type.VaccinationCredential.static-value-map.issuerId=.*@mosip.certify.vciplugin.sunbird-rc.credential-type.VaccinationCredential.static-value-map.issuerId={}@' certify-local.properties
+
+  echo -n "$2" | xargs -I '{}' sed -i -E 's@mosip.esignet.vciplugin.sunbird-rc.credential-type.VaccinationCredential.cred-schema-id=.*@mosip.esignet.vciplugin.sunbird-rc.credential-type.VaccinationCredential.cred-schema-id={}@' esignet-local.properties
+  echo -n "$2" | xargs -I '{}' sed -i -E 's@mosip.certify.vciplugin.sunbird-rc.credential-type.VaccinationCredential.cred-schema-id=.*@mosip.certify.vciplugin.sunbird-rc.credential-type.VaccinationCredential.cred-schema-id={}@' certify-local.properties
+
+  kubectl rollout restart deploy inji-certify -n esignet
+  kubectl rollout restart deploy esignet -n esignet
+}
+
+get_input() {
+    local prompt="$1"
+    read -p "$prompt : " input_value
+    echo "$input_value"
+}
+
+did=$(get_input "Enter the DID id generated")
+schema=$(get_input "Enter the schema id generated")
+
+update_did $did $schema
---
+++ upload_p12.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+upload_p12(){
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+  nvm install 20
+  node -v
+  npm -v
+  openssl version
+  npm install -g cli-jwk-to-pem
+  jwk=$1
+  jwk-to-pem --jwk $jwk > key.pem
+
+  openssl req -new -sha256 -key key.pem -out csr.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+
+  openssl req -x509 -sha256 -days 365 -key key.pem -in csr.csr -out certificate.pem
+
+  export pass=$(cat ~/azure-devops/mimoto-default.properties | grep "mosip.oidc.p12.password" | sed  -e 's/mosip.oidc.p12.password=//g')
+  openssl pkcs12 -export -out oidckeystore.p12 -inkey key.pem -in certificate.pem --name esignet-sunbird-partner -passout env:pass
+
+  kubectl create secret generic mimotooidc -n esignet --from-file=oidckeystore.p12
+  kubectl rollout restart deploy mimoto -n esignet
+}
+
+get_input() {
+    local prompt="$1"
+    read -p "$prompt : " input_value
+    echo "$input_value"
+}
+
+pkey=$(get_input "Enter the private key")
+
+upload_p12 $pkey
