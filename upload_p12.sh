8b5214c7a819fb68622cf92eae32b13252ad1811
tejash-jl <tejash.jl1020@gmail.com>
2024-10-17 23:39:46 +0530

    Fix nvm installation
    Signed-off-by: tejash-jl <tejash.jl1020@gmail.com>

--- upload_p12.sh
+++ upload_p12.sh
@@ -2,12 +2,15 @@
 
 upload_p12(){
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   nvm install 20
   node -v
   npm -v
   openssl version
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
   npm install -g cli-jwk-to-pem
   jwk=$1
   jwk-to-pem --jwk $jwk > key.pem
